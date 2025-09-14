e28627d26757b5d8ed36c395d7f8cecfcda777cc
Aathi-sh <aathi2005ug@gmail.com>
2025-09-14 15:26:10 +0530

    changed all the requirements

--- create_puzzles/models.py
+++ create_puzzles/models.py
@@ -11,7 +11,7 @@ class Item(models.Model):
 
     name = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
+    author = models.CharField(max_length=100,null=True,blank=True,default='Admin')
     difficulty = models.CharField(max_length=20, choices=DIFFICULTY_CHOICES)
     row = models.SmallIntegerField(validators=[MinValueValidator(1),MaxValueValidator(20)],
                                    help_text="Enter a number between 1 and 20")
@@ -25,10 +25,8 @@ class Item(models.Model):
     # Automatically set every time the object is saved
     updated_at = models.DateTimeField(auto_now=True)
     
-    created_by = models.ForeignKey (User,related_name='created_by_items',
-    on_delete =models.SET_NULL,null=True,blank=True)
-    updated_by = models.ForeignKey(User,related_name='updated_by_items',
-    on_delete =models.SET_NULL,null=True,blank=True)
+    created_by = models.IntegerField(null=True,blank=True)
+    updated_by = models.IntegerField(null=True,blank=True)
     active = models.BooleanField(default=True)
 
     def _str_(self):

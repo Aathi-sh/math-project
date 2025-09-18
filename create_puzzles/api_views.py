0550c133518030805c0bbb6ed133fca9807904dc
Aathi-sh <aathi2005ug@gmail.com>
2025-09-18 13:59:05 +0530

    updated the dta filterizing

--- create_puzzles/api_views.py
+++ create_puzzles/api_views.py
@@ -1,12 +1,28 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import Item
 from rest_framework.serializers import ModelSerializer
 
-class TempSerializer(ModelSerializer):
+class ItemSerializer(ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
-        
+
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
-    serializer_class = TempSerializer        
+    serializer_class = ItemSerializer
+
+    # Override retrieve to handle both ID and difficulty level
+    def retrieve(self, request, pk=None):
+     
+        if pk.isdigit():
+            return super().retrieve(request, pk)
+        
+        elif pk.lower() in ["easy", "medium", "hard"]:
+            items = Item.objects.filter(difficulty__iexact=pk)
+            serializer = self.get_serializer(items, many=True)
+            return Response(serializer.data)
+        
+     
+        else:
+            return Response({"error": "Invalid parameter"}, status=400)
